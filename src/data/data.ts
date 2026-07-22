433fcb573d78b6abb140f6095eaac1f40a54772d
Mateo Elias <oscarmateoelias@gmail.com>
2026-07-22 11:52:58 -0600

    Libros extra

---
+++ src/data/data.ts
@@ -0,0 +1,74 @@
+import { Product } from "../types/Product";
+
+export const books: Product[] = [
+  {
+    id: 1,
+    title: 'Cien años de soledad',
+    price: 100,
+    image: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity: 1
+  },
+  {
+    id: 2,
+    title: 'El señor de los anillos (Trilogía)',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 3,
+    title: 'Cuentos de Barro',
+    price: 30,
+    image: 'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+    quantity: 1
+  },
+  {
+    id: 4,
+    title: 'Tierra de Infancia',
+    price: 30,
+    image: 'https://assets.isu.pub/document-structure/230605011415-591c5b47dde190303e7de29a0a888041/v1/fa8a209bb2ce609861c6572f117e69f8.jpeg?width=720&quality=85%2C50',
+    quantity: 1
+  },
+  {
+    id: 5,
+    title: 'Harry Potter Pack',
+    price: 390,
+    image: 'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+    quantity: 1
+  },
+  {
+  id: 6,
+  title: "Pedro Páramo",
+  price: 45,
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/0d/Pedro_Paramo_-_first_edition_cover.jpg",
+  quantity: 1
+},
+{
+  id: 7,
+  title: "Don Quijote de la Mancha",
+  price: 120,
+  image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Don_Quijote_de_la_Mancha_%281%29.jpg",
+  quantity: 1
+},
+{
+  id: 8,
+  title: "La sombra del viento",
+  price: 95,
+  image: "https://upload.wikimedia.org/wikipedia/en/0/0b/La_sombra_del_viento.jpg",
+  quantity: 1
+},
+{
+  id: 9,
+  title: "1984",
+  price: 80,
+  image: "https://upload.wikimedia.org/wikipedia/en/c/c3/1984first.jpg",
+  quantity: 1
+},
+{
+  id: 10,
+  title: "El Principito",
+  price: 60,
+  image: "https://upload.wikimedia.org/wikipedia/en/0/05/Littleprince.JPG",
+  quantity: 1
+}
+];
